8fb1e0d730f080c967805fc00b38a8f2e4ebe717
Richard <xqtwkk@gmail.com>
2023-03-25 01:28:08 +0300

    done

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList arrayList = new ArrayList();
+        for (int i = 1; i < sourceList.size() + 1 ; i++) {
+            if (i % 3 == 0) {
+                arrayList.add(sourceList.get(i-1));
+                arrayList.add(sourceList.get(i-1));
+            }
+        }
+        return arrayList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,26 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int x1 = Integer.parseInt(a);
+        int x2 = Integer.parseInt(b);
+        int f1 = 5*x1*x1 + 3;
+        int f2 = 5*x2*x2 + 3;
+        if (f1 == f2) {
+            return x1 - x2;
+        }
+        return f1 - f2;
     }
-}
+    }
+
